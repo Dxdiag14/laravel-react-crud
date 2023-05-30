995647acc65c05b00695602b584d50cd3397d2d0
HJ S. Miranda <hmiranda@moylanmnl.net>
2023-05-30 09:39:57 +0800

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //add thee ff route to the existing routes bcoz we want the posts route accessible to authenticate users only.
+    //we'll use a resource route bcoz it contains all the exact routes we need for a typical CRUD app.
+    Route::resource('posts', PostController::class);
+}); 
+
+require __DIR__.'/auth.php';
